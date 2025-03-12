79fc5d7a102f078be49f297449ff35e6ff28e4db
IvannIvannov <ivann.ivannov26@icloud.com>
2025-03-12 13:10:37 +0200

    extract some components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+
 import './App.css'
 
 function App() {
 
     return (
         <div id="box">
-            
+            <Header />
+
+            {/* <!-- Main Content --> */}
+            <main id="main-content">
+                <Home />
+            </main>
         </div>
     )
 }
