b84be193dd47b6cbd774cad5d5dbf03eae43d712
IvannIvannov <ivann.ivannov26@icloud.com>
2025-03-17 15:09:47 +0200

    show comments

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -3,7 +3,16 @@ import request from "../utils/request";
 const baseUrl = 'http://localhost:3030/jsonstore/comments';
 
 export default {
+   async getAll(gameId){
+        const comments = await request.get(baseUrl);
+
+        // TODO: Filter when migrate to collections
+
+        const gameComments = Object.values(comments).filter(comment => comment.gameId === gameId);
+
+        return gameComments;
+    },
     create(email, gameId, comment) {
         return request.post(baseUrl, { email, gameId, comment });
-    }
+    },
 };
